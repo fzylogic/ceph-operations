728e411700c89a85c7b6173dd99a738b331eef94
Jeremy Hanmer <jeremy@dreamhost.com>
2014-03-10 17:18:36 +0000

    handle the case where PGs are active+clean+somethingelse this mainly prevents false positives with active+clean+scrubbing+deep

--- nagios/check_ceph_pg_activeclean.pl
+++ nagios/check_ceph_pg_activeclean.pl
@@ -5,14 +5,19 @@ use strict;
 open(CEPH, "/usr/bin/ceph pg stat|");
 my $pg_stat = <CEPH>;
 close(CEPH);
-my ($total_pgs, $active_pgs) = $pg_stat =~ /([0-9]+) pgs: ([0-9]+) active\+clean/;
+my ($total_pgs) = $pg_stat =~ /([0-9]+) pgs:/;
+my (@active_pgs) = $pg_stat =~ /([0-9]+) active\+clean/g;
+my $active;
+foreach my $a (@active_pgs) {
+  $active += $a;
+}
 
-if ( $total_pgs == $active_pgs ) {
-  print "All active+clean\n";
+if ( $total_pgs == $active ) {
+  print 'OK: ' . $pg_stat;
   exit(0);
 }
-elsif ( $total_pgs > $active_pgs ) {
-  print "Some non-clean PGs exist!\n";
+elsif ( $total_pgs > $active ) {
+  print 'CRIT: ' . $pg_stat;
   exit(2);
 }
 else {
